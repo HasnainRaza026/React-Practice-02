e2f9183c08dfda359299d15f1cef11193fa76e5e
HasnainRaza026 <hasnainraza0026@gmail.com>
2025-03-23 07:10:38 +0500

    optimized the workout timmer project

---
+++ workout-timer/DETAIL.md
@@ -0,0 +1,3 @@
+# Workout Timmer
+
+I this project i tried something different, I was given a code base with all the code already written. And I optimized the code for better performance
---
+++ workout-timer/src/ToggleSounds.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ToggleSounds({ allowSound, setAllowSound }) {
+  return (
+    <button
+      className="btn-sound"
+      onClick={() => setAllowSound((allow) => !allow)}
+    >
+      {allowSound ? "🔈" : "🔇"}
+    </button>
+  );
+}
+
+export default React.memo(ToggleSounds);
